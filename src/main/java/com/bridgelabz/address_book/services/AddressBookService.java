18f99357fc7bb3db08b0aad487632b8c384b0d7f
amruta.vatare <vatareamruta615@gmail.com>
2022-11-05 01:10:47 +0530

    implemented an Java mail sender utility class to send token on mail

--- src/main/java/com/bridgelabz/address_book/services/AddressBookService.java
+++ src/main/java/com/bridgelabz/address_book/services/AddressBookService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.bridgelabz.address_book.advice.AddressBookException;
 import com.bridgelabz.address_book.repository.IAddressBookRepository;
 import com.bridgelabz.address_book.repository.model.ContactData;
+import com.bridgelabz.address_book.security.EmailSenderService;
 import com.bridgelabz.address_book.security.JwtUtil;
 import com.bridgelabz.address_book.services.model.ContactDTO;
 import com.bridgelabz.address_book.services.model.Mapper;
@@ -20,6 +21,9 @@ public class AddressBookService implements IAddressBookService {
     private IAddressBookRepository repository;
 
     @Autowired
+    EmailSenderService senderService;
+
+    @Autowired
     private JwtUtil util;
 
     @Override
@@ -27,6 +31,11 @@ public class AddressBookService implements IAddressBookService {
         ContactData contact = Mapper.toRepository(dto);
         repository.save(contact);
         String token = util.generateToken(contact.getId());
+        try{
+            senderService.sendEmail(contact.getEmail(), "Token authentication", "Hello, "+contact.getFullName()+"\n"+token);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         return token;
     }
 
